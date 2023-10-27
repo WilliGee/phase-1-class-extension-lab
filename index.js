e0bccb0709366f36fb8e662d688e11a04982e43a
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2023-10-27 09:19:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+        return this.array.length
+    }
+    get perimeter(){
+        let tt=0;
+        for(let i=0;i<this.array.length; i++){
+            tt+= this.array[i];
+        }
+        return tt;
+
+
+}}
+class Triangle extends Polygon{
+   // super(array)
+    get isValid(){
+        if(this.array[0]+ this.array[1] > this.array[2] && this.array[1]+this.array[2] > this.array[0] && this.array[2] + this.array[0] > this.array[1])
+        return true; 
+        return false;
+    }
+    
+}
+class Square extends Polygon{
+    get isValid(){
+        return (this.array[0] ===this.array[1]) ? true : false;
+    }
+    get area(){
+        return Math.pow(this.array[0],2)
+
+    }
+}
